72fd804d736c600a66ddd4851f57cd1d50610372
TreyMorrison <30305709+TreyMorrison@users.noreply.github.com>
2018-03-20 19:05:45 -0400

    Add files via upload

---
+++ seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO rapStats (name, uWords, uToArtist, numSongs, wordsPerSong, imgURL, uWordsProp, mysteryNumSongs, wordsPerSongProp)
+VALUES ('KRS One', 400, 17, 62, 137.4, "bit.ly/hjsdgjhasf", 1.2, 14.4, 177.1);
+
+INSERT INTO rapStats (name, uWords, uToArtist, numSongs, wordsPerSong, imgURL, uWordsProp, mysteryNumSongs, wordsPerSongProp)
+VALUES ('Mc Lyte', 500, 22, 52, 134.6, "bit.ly/jhasdkfjfgo", 1.3, 13.8, 160.4);
