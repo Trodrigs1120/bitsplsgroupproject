84212ee7c126acd6ec8b29ddc7aa76b76eab9db2
Shane Scott <shaneradio@gmail.com>
2018-03-22 18:02:34 -0400

    more changes for handlebars additions

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,19 +1,6 @@
 // This .on("click") function will trigger the AJAX Call
       
 
-
-
-      $("#findRapStats").on("click", function(event) {
-
-        var stats = $(this).attr("rap-stats");//
-
-        // event.preventDefault() can be used to prevent an event's default behavior.
-        // Here, it prevents the submit button from trying to submit a form when clicked
-        event.preventDefault();
-
-        // Here we grab the text from the input box
-        var artistInput = $("#rapper-input").val().trim();
-
       $(document).ready (function () {
 
         var source = $("#statsTemplate").html ();
@@ -33,5 +20,22 @@
           percentUnique: "some.queried.data.from.db.and.orm",
         }
 
+        var new_html = template (context);
+
+        $("statsTemplate").html (new_html);
 
     });
+
+      $("#findRapStats").on("click", function(event) {
+
+        var stats = $(this).attr("rap-stats");//
+
+        // event.preventDefault() can be used to prevent an event's default behavior.
+        // Here, it prevents the submit button from trying to submit a form when clicked
+        event.preventDefault();
+
+        // Here we grab the text from the input box
+        var artistInput = $("#rapper-input").val().trim();
+
+      
+
